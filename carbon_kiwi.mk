71f1eedfd7e48748fc43f1b5999aac78ad149090
Norris, Cisco <cisco.norris@linux.com>
2017-02-24 22:22:35 +0100

    kiwi: Carbonify.
    Change-Id: Ife5ab3f8fcd65a943ba756f7552bc5fbe1b2983

--- carbon_kiwi.mk
+++ carbon_kiwi.mk
@@ -14,10 +14,13 @@
 
 $(call inherit-product, device/huawei/kiwi/full_kiwi.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit Carbon GSM telephony parts
+$(call inherit-product, vendor/carbon/config/gsm.mk)
+  
+# Inherit Carbon product configuration
+$(call inherit-product, vendor/carbon/config/common.mk)
 
-PRODUCT_NAME := lineage_kiwi
+PRODUCT_NAME := carbon_sumire
 BOARD_VENDOR := huawei
 
 PRODUCT_GMS_CLIENTID_BASE := android-huawei
