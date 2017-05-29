3aa7f5332b74eb074a76bd1c8d7614ceffb6db95
Cisco R <cisco.norris@linux.com>
2017-05-28 23:32:20 -0400

    kiwi: Carbon-ify the device tree.
    Change-Id: Ib1a026712a91d6696aabde16f2e29a9ceef395f1

--- doze/src/org/carbonrom/settings/doze/DozeSettings.java
+++ doze/src/org/carbonrom/settings/doze/DozeSettings.java
@@ -14,7 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.cyanogenmod.settings.doze;
+package org.carbonrom.settings.doze;
 
 import android.os.Bundle;
 import android.view.MenuItem;
